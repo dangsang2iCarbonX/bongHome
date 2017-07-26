289a1d5f844f2e03715b3e5e0e92744c3e97c60f
hackill <hankuo@bong.cn>
2017-07-26 20:19:49 +0800

    add lib

--- app/src/main/java/cn/bong/bonghome/MainActivity.java
+++ app/src/main/java/cn/bong/bonghome/MainActivity.java
@@ -1,13 +1,124 @@
 package cn.bong.bonghome;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.tuesda.walker.circlerefresh.CircleRefreshLayout;
+
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    private CircleRefreshLayout mRefreshLayout;
+    private ListView mList;
+    private Button mStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRefreshLayout = (CircleRefreshLayout) findViewById(R.id.refresh_layout);
+        mList = (ListView) findViewById(R.id.list);
+        mStop = (Button) findViewById(R.id.stop_refresh);
+
+        String[] strs = {
+                "The",
+                "Canvas",
+                "class",
+                "holds",
+                "the",
+                "draw",
+                "calls",
+                ".",
+                "To",
+                "draw",
+                "something,",
+                "you",
+                "need",
+                "4 basic",
+                "components",
+                "Bitmap",
+                "The",
+                "Canvas",
+                "class",
+                "holds",
+                "the",
+                "draw",
+                "calls",
+                ".",
+                "To",
+                "draw",
+                "something,",
+                "you",
+                "need",
+                "4 basic",
+                "components",
+                "Bitmap",
+                "The",
+                "Canvas",
+                "class",
+                "holds",
+                "the",
+                "draw",
+                "calls",
+                ".",
+                "To",
+                "draw",
+                "something,",
+                "you",
+                "need",
+                "4 basic",
+                "components",
+                "Bitmap",
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strs);
+        mList.setAdapter(adapter);
+
+        mStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRefreshLayout.finishRefreshing();
+            }
+        });
+
+        mRefreshLayout.setOnRefreshListener(
+                new CircleRefreshLayout.OnCircleRefreshListener() {
+                    @Override
+                    public void refreshing() {
+                        // do something when refresh starts
+                        Log.i(TAG, "refreshing: ....");
+
+//                        mRefreshLayout.postDelayed(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                mRefreshLayout.finishRefreshing();
+//                            }
+//                        }, 3000);
+                    }
+
+                    @Override
+                    public void completeRefresh() {
+                        // do something when refresh complete
+                        Log.i(TAG, "completeRefresh: ");
+                    }
+                });
+
+
+        findViewById(R.id.go).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/cn/bong/bonghome/TestActivity.java
@@ -0,0 +1,78 @@
+package cn.bong.bonghome;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.tuesda.walker.circlerefresh.Content;
+import com.tuesda.walker.circlerefresh.CycProgressBar;
+
+/**
+ * @author hackill
+ * @date on 17/7/25 17:15
+ */
+
+public class TestActivity extends Activity {
+
+
+    CycProgressBar cycProgressBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.test);
+
+        cycProgressBar = (CycProgressBar) findViewById(R.id.cycbar);
+
+        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                float progress = cycProgressBar.getProgress();
+
+                cycProgressBar.setProgress(progress + 1);
+            }
+        });
+
+        findViewById(R.id.less).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                float progress = cycProgressBar.getProgress();
+                cycProgressBar.setProgress(progress - 1);
+            }
+        });
+
+
+        findViewById(R.id.all).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Content content = new Content();
+
+                content.addition = "233";
+                content.additionUnit = "大卡";
+                content.value = "9999";
+                content.unit = "步";
+                content.imgRes = R.drawable.icon_sleep;
+
+                cycProgressBar.setContent(content, CycProgressBar.Style.Sleep);
+
+            }
+        });
+
+        findViewById(R.id.simple).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Content content = new Content();
+
+                content.value = "23120";
+                content.unit = "步";
+                cycProgressBar.setContent(content, CycProgressBar.Style.Thumbnail);
+            }
+        });
+
+    }
+}
